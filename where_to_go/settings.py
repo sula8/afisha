1aa4f794c5fbe246c930dddc9f8e1fd7955a2c4d
Sultanbek Altayev <sultanbekaltayev8@gmail.com>
2020-07-06 18:33:13 +0100

    fix static root and dirs

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -110,7 +110,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATIC_ROOT = os.getenv('STATIC_ROOT')
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
