8968330484f59bebf30ac58c2dbe267d15229130
Sultanbek Altayev <sultanbekaltayev8@gmail.com>
2020-07-06 18:33:26 +0100

    fix tab issue

--- places/models.py
+++ places/models.py
@@ -5,34 +5,32 @@ from tinymce.models import HTMLField
 
 
 class Place(models.Model):
-	title = models.CharField("Название", max_length=200)
-	description_short = models.CharField("Короткое описание", max_length=600)
-	description_long = HTMLField("Описание")
-	lng = models.FloatField("Долгота")
-	lat = models.FloatField("Широта")
+    title = models.CharField("Название", max_length=200)
+    description_short = models.CharField("Короткое описание", max_length=600)
+    description_long = HTMLField("Описание")
+    lng = models.FloatField("Долгота")
+    lat = models.FloatField("Широта")
 
-	def __str__(self):
-		return f'{self.title}'
+    def __str__(self):
+        return f'{self.title}'
 
-	class Meta:
-		verbose_name = 'Место'
-		verbose_name_plural = 'Места'
+    class Meta:
+        verbose_name = 'Место'
+        verbose_name_plural = 'Места'
 
 
 class PlaceImage(models.Model):
-	place = models.ForeignKey(Place, on_delete=models.CASCADE, related_name='images', verbose_name="Изображение места")
-	image = models.ImageField("Изображение")
-	my_order = models.PositiveIntegerField(default=0, blank=False, null=False, verbose_name="Позиция")
+    place = models.ForeignKey(Place, on_delete=models.CASCADE, related_name='images', verbose_name="Изображение места")
+    image = models.ImageField("Изображение")
+    my_order = models.PositiveIntegerField(default=0, blank=False, null=False, verbose_name="Позиция")
 
-	def __str__(self):
-		return f'Изображение #{self.id} места "{self.place.title}"'
+    def __str__(self):
+        return f'Изображение #{self.id} места "{self.place.title}"'
 
-	def image_preview(self):
-		return format_html('<img src="{url}" style="height:100px" />'.format(url=self.image.url))
+    def image_preview(self):
+        return format_html('<img src="{url}" style="height:100px" />'.format(url=self.image.url))
 
-	class Meta:
-		ordering = ['my_order']
-		verbose_name = 'Изображение места'
-		verbose_name_plural = 'Изображения места'
-
-
+    class Meta:
+        ordering = ['my_order']
+        verbose_name = 'Изображение места'
+        verbose_name_plural = 'Изображения места'
